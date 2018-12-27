8f7b5f9a1d31eea082c1623b3410c5514bb3d463
Reijer Knol <rknol@nlmb000105.home>
2018-12-27 13:05:54 +0100

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Knol, Reijer (NL - Amsterdam) on 27/12/2018.
+//  Copyright © 2018 Knol, Reijer (NL - Amsterdam). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
